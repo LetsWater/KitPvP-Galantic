c14a396cb16df1cb078986ee4bd8f5462d4d7328
NormaalBart <bartkempen10@gmail.com>
2019-11-28 20:34:22 +0100

    added playertoggleflightlistener

--- main/java/me/flame/galantic/listeners/MoveEvent.java
+++ main/java/me/flame/galantic/listeners/MoveEvent.java
@@ -4,6 +4,8 @@ import me.flame.galantic.kits.KitManager;
 import me.flame.galantic.sql.SQLUser;
 import me.flame.galantic.sql.managers.SQLUserManager;
 import me.flame.galantic.utils.FileManager;
+
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,19 +13,22 @@ import org.bukkit.event.player.PlayerMoveEvent;
 
 public class MoveEvent implements Listener {
 
-    @EventHandler
-    public void onMove(final PlayerMoveEvent e){
-        Player p = e.getPlayer();
+	@EventHandler
+	public void onMove( final PlayerMoveEvent e ) {
+		Player p = e.getPlayer();
 
-        String locatie = FileManager.get("config.yml").getString("spawn.locatie");
-        String[] loc = locatie.split(";");
-        if(p.getLocation().getY() <= Double.parseDouble(loc[2]) - 3 && p.getLocation().getY() >= Double.parseDouble(loc[2]) - 5 && p.getInventory().getHelmet() == null){
-            for(SQLUser user : SQLUserManager.userList){
-                if(user.getUuid() == p.getUniqueId()){
-                    KitManager.getInstance().giveKit(p.getUniqueId(), user.getUsing_kit());
-                    break;
-                }
-            }
-        }
-    }
+		String locatie = FileManager.get( "config.yml" ).getString( "spawn.locatie" );
+		String[] loc = locatie.split( ";" );
+		if ( p.getLocation().getY() <= Double.parseDouble( loc[ 2 ] ) - 3
+				&& p.getLocation().getY() >= Double.parseDouble( loc[ 2 ] ) - 5
+				&& p.getInventory().getHelmet() == null && p.getGameMode() == GameMode.ADVENTURE ) {
+			for ( SQLUser user : SQLUserManager.userList ) {
+				if ( user.getUuid() == p.getUniqueId() ) {
+					p.setAllowFlight( false );
+					KitManager.getInstance().giveKit( p.getUniqueId(), user.getUsing_kit() );
+					break;
+				}
+			}
+		}
+	}
 }
